0cf945aaf4c064cb3e4ddb90abb68edf800dfb8d
jonbaird <jbaird29@gmail.com>
2021-10-23 13:56:21 -0400

    check for empty lines or comments

--- main.c
+++ main.c
@@ -1,23 +1,61 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, char *argv[]) {
-  char input[2049];  // max length of 2048 characters
+struct commandText {
+  char *command;
+  char *arguments[512];
+  char *inpuFile;
+  char *outputFile;
+  bool isBackground;
+};
+
+
+void parseCommandText(char *input) {
+  return;
+}
+
+
+// Returns true if the first printable character in 'text' is 'comparison'
+// Also returns true if 'text' is comprised of only non printable characters (eg spaces or newlines)
+// Otherwsies returns false
+bool startsWithOrEmpty(char * text, char comparison) {
+  int i = 0;
+  while(text[i] != '\0') {
+    if(text[i] == comparison) {
+        // if this letter is 'comparison' value, return true
+      return true;
+    } else if(text[i] >= 33 && text[i] <= 126) {
+       // if this letter is a printable character, return false (does not start with comp)
+      return false;
+    }
+    // if this letter is a nonprintable character, iterate to next letter
+    i++;
+  }
+  // if ALL letters were non printable, return true
+  return true;
+}
+
+
+int main() {
+  char commandText[2049];  // max length of 2048 characters
   while(1) {
+    memset(commandText, '\0', sizeof(commandText));
     putchar(':');
     putchar(' ');
-    fgets(input, 2049, stdin);
+    fflush(stdout);
+    fgets(commandText, 2049, stdin);
     // Below line copied from: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
     // Date: 10/22/2021
     // How to remove newline character after fgets() 
-    input[strcspn(input, "\n")] = 0;
-    if(strcmp(input, "exit") == 0) {
+    commandText[strcspn(commandText, "\n")] = 0;
+    if(strcmp(commandText, "exit") == 0) {
       // if command was "exit" then stop the program
       return EXIT_SUCCESS;
-    } else {
-      // TBU - pass this into command parsing function
-      printf("%s\n", input);
+    } else if(!startsWithOrEmpty(commandText, '#')) { 
+      // if command starts with '#' or is empty, do nothing
+      printf("%s\n", commandText);
     }
   }
 }
